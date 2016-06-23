164fc6afa9b1bd80877b871eff93a33d87d12523
Mc.Queide <mcqueide@gmail.com>
2016-06-23 13:57:23 -0300

    Assignment 3: Single Page Applications -Task 1

--- cousera-angular/conFusion_gulp/app/scripts/controllers.js
+++ cousera-angular/conFusion_gulp/app/scripts/controllers.js
@@ -2,17 +2,18 @@
 
 angular.module('confusionApp')
 
-        .controller('MenuController', ['$scope', 'menuFactory', function($scope,menuFactory) {
-
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes = menuFactory.getDishes();
+            $scope.dishes= menuFactory.getDishes();
 
+                        
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
-
+                
                 if (setTab === 2) {
                     $scope.filtText = "appetizer";
                 }
@@ -30,7 +31,7 @@ angular.module('confusionApp')
             $scope.isSelected = function (checkTab) {
                 return ($scope.tab === checkTab);
             };
-
+    
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
@@ -39,20 +40,20 @@ angular.module('confusionApp')
         .controller('ContactController', ['$scope', function($scope) {
 
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-
+            
             var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-
+            
             $scope.channels = channels;
             $scope.invalidChannelSelection = false;
-
+                        
         }])
 
         .controller('FeedbackController', ['$scope', function($scope) {
-
+            
             $scope.sendFeedback = function() {
-
+                
                 console.log($scope.feedback);
-
+                
                 if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
@@ -67,45 +68,32 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$stateParams','menuFactory', function($scope,$stateParams,menuFactory) {
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish = menuFactory.getDish(parseInt($stateParams.id,10));
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
             $scope.dish = dish;
-
+            
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
-
-            //Step 1: Create a JavaScript object to hold the comment from the form
-            $scope.newComment={
-                rating:5,
-                comment:"",
-                author:"",
-                date:""
-            };
-
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
             $scope.submitComment = function () {
-
-                //Step 2: This is how you record the date
-                //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                $scope.newComment.date = new Date().toISOString();
-                // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push($scope.newComment);
-
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.dish.comments.push($scope.mycomment);
+                
                 $scope.commentForm.$setPristine();
-
-                $scope.newComment={
-                    rating:5,
-                    comment:"",
-                    author:"",
-                    date:""
-                };
-
-                //Step 4: reset your form to pristine
-
-                //Step 5: reset your JavaScript object that holds your comment
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
             }
         }])
+
+        // implement the IndexController and About Controller here
+
 
 ;

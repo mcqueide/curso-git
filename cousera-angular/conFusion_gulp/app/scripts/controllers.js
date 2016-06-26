bd528cf496ab282334f88d89cdb0ddee0729b570
mcqueide <mcqueide@gmail.com>
2016-06-26 11:06:00 -0300

    Handling Erros with $resource Adding put operation

--- cousera-angular/conFusion_gulp/app/scripts/controllers.js
+++ cousera-angular/conFusion_gulp/app/scripts/controllers.js
@@ -8,9 +8,16 @@ angular.module('confusionApp')
         $scope.filtText = '';
         $scope.showDetails = false;
 
-        $scope.showMenu = true;
+        $scope.showMenu = false;
         $scope.message = "Loading ...";
-        $scope.dishes = menuFactory.getDishes().query();
+        menuFactory.getDishes().query(
+            function(response) {
+                $scope.dishes = response;
+                $scope.showMenu = true;
+            },
+            function(response) {
+                $scope.message = "Error: "+response.status + " " + response.statusText;
+            });
 
 
         $scope.select = function (setTab) {
@@ -73,26 +80,33 @@ angular.module('confusionApp')
     .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function ($scope, $stateParams, menuFactory) {
 
         $scope.dish = {};
-        $scope.showDish = true;
+        $scope.showDish = false;
         $scope.message="Loading ...";
-        $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+        $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
 
     }])
 
-    .controller('DishCommentController', ['$scope', function ($scope) {
+    .controller('DishCommentController', ['$scope','menuFactory', function ($scope,menuFactory) {
 
         $scope.mycomment = {rating: 5, comment: "", author: "", date: ""};
 
         $scope.submitComment = function () {
-
             $scope.mycomment.date = new Date().toISOString();
             console.log($scope.mycomment);
-
             $scope.dish.comments.push($scope.mycomment);
 
+            menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
             $scope.commentForm.$setPristine();
-
-            $scope.mycomment = {rating: 5, comment: "", author: "", date: ""};
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
         }
     }])
 
@@ -103,10 +117,18 @@ angular.module('confusionApp')
 
         $scope.leader = corporateFactory.getLeader(3);
 
-        $scope.dish = {};
-        $scope.showDish = true;
+        $scope.showDish = false;
         $scope.message="Loading ...";
-        $scope.dish = menuFactory.getDishes().get({id:0});
+        $scope.dish = menuFactory.getDishes().get({id:0})
+            .$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
     }])
 
     .controller('AboutController', ['$scope', 'corporateFactory', function ($scope, corporateFactory) {

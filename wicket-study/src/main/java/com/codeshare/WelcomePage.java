2f6b9843e4cb435bbd86d157e1ecc248e964f8a8
mcqueide <mcqueide@gmail.com>
2016-07-17 11:40:17 -0300

    Second class

--- wicket-study/src/main/java/com/codeshare/WelcomePage.java
+++ wicket-study/src/main/java/com/codeshare/WelcomePage.java
@@ -1,16 +1,54 @@
 package com.codeshare;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+
+import com.codeshare.model.UserModel;
 
 public class WelcomePage extends WebPage {
 
 	private static final long serialVersionUID = -8903220313226134259L;
 	
+	private List<String> genderChoices = new ArrayList<String>();
+	
 	public WelcomePage() {
-		add(new Label("lbl", 
-			new Model<String>("This is my first Wicket App")));
+		genderChoices.add("Male");
+		genderChoices.add("Female");
+		final UserModel userModel = new UserModel();
+		
+		Form<?> form = new Form("form");
+		
+		TextField<String> text = new TextField<String>("text",
+			new PropertyModel<String>(userModel, "nome"));
+		
+		DropDownChoice<String> gender = 
+			new DropDownChoice<String>("gender", 
+				new PropertyModel<String>(userModel, "gender"), 
+				genderChoices);
+		
+		Button button = new Button("submit"){
+			@Override
+			public void onSubmit() {
+				super.onSubmit();
+				
+				String mensagem = String.format("Name: %s, \n Gender: %s", 
+					userModel.getNome(), userModel.getGender());
+				
+				System.out.println(mensagem);
+			}
+		};
+		
+		add(form);
+		form.add(text);
+		form.add(gender);
+		form.add(button);
 	}
 
 }
---
+++ wicket-study/src/main/java/com/codeshare/model/UserModel.java
@@ -0,0 +1,21 @@
+package com.codeshare.model;
+
+public class UserModel {
+
+	private String nome;
+	private String gender;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}

9c2280766e74d03ba5ddaeac78f16f4fa4cd7247
John Mc Queide Clemente <john.clemente@ctis.local>
2016-07-18 13:39:55 -0300

    Class 4 - Adding panels

--- wicket-study/src/main/java/com/codeshare/WelcomePage.java
+++ wicket-study/src/main/java/com/codeshare/WelcomePage.java
@@ -1,59 +1,13 @@
 package com.codeshare;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.PropertyModel;
-
-import com.codeshare.model.UserModel;
 
 public class WelcomePage extends WebPage {
 
 	private static final long serialVersionUID = -8903220313226134259L;
 	
-	private List<String> genderChoices = new ArrayList<String>();
-	
 	public WelcomePage() {
-		genderChoices.add("Male");
-		genderChoices.add("Female");
-		final UserModel userModel = new UserModel();
-		
-		Form<?> form = new Form("form");
-		
-		final TextField<String> text = new TextField<String>("text",
-			new PropertyModel<String>(userModel, "nome"));
-		text.setOutputMarkupId(true);
-		
-		final DropDownChoice<String> gender = 
-			new DropDownChoice<String>("gender", 
-				new PropertyModel<String>(userModel, "gender"), 
-				genderChoices);
-		gender.setOutputMarkupId(true);
-		
-		AjaxButton ajaxButton = new AjaxButton("submit") {
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				super.onSubmit(target, form);
-				
-				text.setEnabled(false);
-				gender.setEnabled(false);
-				
-				target.add(text);
-				target.add(gender);
-			}
-		};
-		
-		add(form);
-		form.add(text);
-		form.add(gender);
-		form.add(ajaxButton);
+		add(new SamplePanel("panel"));
 	}
 
 }

7ed336102f478346bb56b40f602cd629d790cf09
Mc.Queide <john.clemente@ctis.com.br>
2016-07-22 15:03:34 -0300

    Aula 7

--- alura-javascript/js/adiciona.js
+++ alura-javascript/js/adiciona.js
@@ -1,8 +1,25 @@
-var $pacienteNovo = document.querySelector('.paciente');
 var $table = document.querySelector('table');
 var $botaoAdicionar = document.querySelector('#adicionar-paciente');
+var $form = document.querySelector('form');
 
 $botaoAdicionar.addEventListener('click',function(event) {
   event.preventDefault();
-  $table.innerHTML += $pacienteNovo.innerHTML;
+  var campoNome = document.querySelector("#campo-nome");
+  var campoPeso = document.querySelector("#campo-peso");
+  var campoAltura = document.querySelector("#campo-altura");
+  var campoAltura = document.querySelector("#campo-altura");
+
+  var novoPaciente = "<tr class='paciente'>"+
+                         "<td class='info-nome'>"+campoNome.value+"</td>"+
+                         "<td class='info-peso'>"+campoPeso.value+"</td>"+
+                         "<td class='info-altura'>"+campoAltura.value+"</td>"+
+                         "<td class='info-imc'></td>"+
+                      "</tr>";
+
+  var todosPacientes = document.querySelector("table");
+  todosPacientes.innerHTML += novoPaciente;
+
+  campoNome.value = "";
+  campoPeso.value = "";
+  campoAltura.value = "";
 });

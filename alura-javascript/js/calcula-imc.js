990955fc7ebd6c09e3f2b32a07c8d2a579a607fd
Mc.Queide <john.clemente@ctis.com.br>
2016-07-22 08:49:46 -0300

    Aula 4 - Capturando elementos do DOM pela classe, estrutura de repetição for

--- alura-javascript/js/calcula-imc.js
+++ alura-javascript/js/calcula-imc.js
@@ -1,16 +1,23 @@
-var peso = document.getElementById("peso-2").textContent;
-var altura = document.getElementById("altura-2").textContent;
+var $trPacientes = document.getElementsByClassName("paciente"); //Array de trs
 
-var pessoa2 = {
-	peso : parseFloat(peso),
-	altura : parseFloat(altura)
-}
+for (var posicaoAtual = 0; posicaoAtual < $trPacientes.length; posicaoAtual++) {
+	var $pacienteTr = $trPacientes[posicaoAtual];
 
-if(altura != 0){
-	var imc_calculo = peso / (altura * altura);
-	var $imc = document.getElementById("imc-2");
-	$imc.textContent = imc_calculo;
+	var $tdNome = $pacienteTr.getElementsByClassName("info-nome")[0];
+	var $tdPeso = $pacienteTr.getElementsByClassName("info-peso")[0];
+	var $tdAltura = $pacienteTr.getElementsByClassName("info-altura")[0];
 
-}else{
-	console.log("altura não pode ser 0!");
+	var paciente = {
+		nome : $tdNome.textContent,
+		peso : parseFloat($tdPeso.textContent),
+		altura : parseFloat($tdAltura.textContent)
+	};
+
+	if(paciente.altura != 0){
+		var imc_calculo = paciente.peso / (paciente.altura * paciente.altura);
+		var $imc = $pacienteTr.getElementsByClassName("info-imc")[0];
+		$imc.textContent = imc_calculo;
+	}else{
+		console.log("altura não pode ser 0!");
+	}
 }

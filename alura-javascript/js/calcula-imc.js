af06599701ce75fa17aeade581ca474a2203a9de
Mc.Queide <john.clemente@ctis.com.br>
2016-07-22 08:24:35 -0300

    Aula 3 - Obtendo elementos da tela e sobrescrevendo seus valores

--- alura-javascript/js/calcula-imc.js
+++ alura-javascript/js/calcula-imc.js
@@ -1,9 +1,16 @@
-var peso = 80;
-var altura = 1.80;
+var peso = document.getElementById("peso-2").textContent;
+var altura = document.getElementById("altura-2").textContent;
+
+var pessoa2 = {
+	peso : parseFloat(peso),
+	altura : parseFloat(altura)
+}
 
 if(altura != 0){
-	var imc = peso / (altura * altura);
-	console.log(imc); // imprime 100
+	var imc_calculo = peso / (altura * altura);
+	var $imc = document.getElementById("imc-2");
+	$imc.textContent = imc_calculo;
+
 }else{
 	console.log("altura não pode ser 0!");
 }

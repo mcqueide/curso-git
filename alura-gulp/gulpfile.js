cf2418102bcf5e63c7af341156268ad53eee334d
mcqueide <mcqueide@gmail.com>
2016-07-24 12:46:36 -0300

    Finalizando aula 3 - substituindo concat pelo usemin

--- alura-gulp/gulpfile.js
+++ alura-gulp/gulpfile.js
@@ -1,16 +1,12 @@
 var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    htmlReplace = require('gulp-html-replace');
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin');
 
-gulp.task('default',['copy'],function(){
-  gulp.start('build-img','build-html','build-js','build-css');
-});
-
-gulp.task('copy', ['clean'], function() {
-    return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+gulp.task('default',['clean'],function(){
+  gulp.start('build-img','usemin');
 });
 
 gulp.task('build-img', function() {
@@ -24,27 +20,11 @@ gulp.task('clean',function() {
             .pipe(clean());
 });
 
-gulp.task('build-js', function() {
-    return gulp.src(['src/js/jquery.js',
-              'src/js/home.js',
-              'src/js/ativa-filtro.js'])
-            .pipe(concat('all.js'))
-            .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('build-css',function() {
-  return gulp.src(["src/css/reset.css",
-                "src/css/estilos.css",
-                "src/css/mobile.css"])
-              .pipe(concat('all.css'))
-              .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('build-html', function() {
+gulp.task('usemin', function() {
   return gulp.src('src/**/*.html')
-    .pipe(htmlReplace({
-            'js': 'js/all.js',
-            'css': 'css/all.css',
+    .pipe(usemin({
+      js: [uglify],
+      css: [cssmin]
     }))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist'));
 });

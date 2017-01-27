9718aeaeec7a736adbb4dc92839e1230f0699a3c
mcqueide <mcqueide@gmail.com>
2017-01-27 13:40:09 -0200

    [ADD] Finalizando primeira aula, com os Default Methods das interfaces List(sort) e Interable(forEach).

---
+++ alura-java-8-training/src/main/java/br/com/mcqueide/App.java
@@ -0,0 +1,73 @@
+package br.com.mcqueide;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static List<String> palavras;
+
+	public static void main( String[] args )
+    {
+		palavras = new ArrayList<>();
+    	palavras.add("alura online");
+    	palavras.add("editora casa do codigo");
+    	palavras.add("caelum");
+    	
+        defaultMethods();
+        
+        interableForEach();
+    }
+    
+    public static void defaultMethods(){
+    	Collections.sort(palavras);
+    	System.out.println("Comparador por ondem alfanumérico:\n" + palavras);
+    	
+    	Collections.sort(palavras,new ComparadorPorTamanho());
+    	System.out.println("\nComparador por tamanho de palavra:\n" + palavras);
+    	
+    	palavras.sort(new ComparadorPorTamanho());
+    	System.out.println("\nComparador por tamanho de palavra, mas utilizando o sort da interface List:\n" + palavras);
+    	
+    	System.out.println("\n\n==================================\n\n");
+    }
+    
+    public static void interableForEach(){
+    	System.out.println("For each tradicional:");
+    	for (String palavra : palavras) {
+			System.out.print(palavra + ", ");
+		}
+    	
+    	System.out.println("\n\nFor each da interface interable:");
+    	palavras.forEach(new ImprimeNaLinha());
+    }
+}
+
+class ComparadorPorTamanho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		if(s1.length() < s2.length())
+			return -1;
+		if(s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+	
+}
+
+class ImprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String s) {
+		System.out.print(s + ", ");
+	}
+	
+}

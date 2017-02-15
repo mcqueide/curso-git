f6dff41825769fa13882b6317441147bdcab3a6d
mcqueide <mcqueide@gmail.com>
2017-02-15 13:14:54 -0200

    Adicionando leitura de listas com stream

--- alura-java-8-training/src/main/java/br/com/mcqueide/Aula4.java
+++ alura-java-8-training/src/main/java/br/com/mcqueide/Aula4.java
@@ -1,6 +1,7 @@
 package br.com.mcqueide;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -15,5 +16,50 @@ public class Aula4 {
         cursos.add(new Curso("JavaScript", 150));
         cursos.add(new Curso("Java 8", 113));
         cursos.add(new Curso("C", 55));
+
+        ordenaCursosPorQuantidadeAlunos(cursos);
+
+        System.out.println("Ordena lista: ");
+        cursos.forEach(curso -> System.out.println(curso.getNome()));
+
+        System.out.println("\nLista Com Stream: ");
+        listaComStream(cursos);
+
+        System.out.println("\nLista com map: ");
+        listaComStreamEMap(cursos);
+
+        System.out.println("\nLista com map retornando soma de alunos de cursos com mais de 100 alunos: ");
+        listaComStreamEMapSomandoTotalDeAlunosDeCursosComMaisDe100(cursos);
     }
+
+    public static void ordenaCursosPorQuantidadeAlunos(List<Curso>cursos){
+        //com lambda
+        //cursos.sort(Comparator.comparingInt(c -> c.getAlunos()));
+        //com method reference
+        cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+    }
+
+    public static void listaComStream(List<Curso> cursos){
+        cursos.stream()
+                .filter(curso -> curso.getAlunos() > 100)
+                .forEach(c -> System.out.println(c.getNome()));
+    }
+
+    public static void listaComStreamEMap(List<Curso> cursos){
+        cursos.stream()
+                .filter(curso -> curso.getAlunos() > 100)
+                .map(curso -> curso.getAlunos())
+                .forEach(System.out::println);
+    }
+
+    public static void listaComStreamEMapSomandoTotalDeAlunosDeCursosComMaisDe100(List<Curso> cursos){
+        int sum = cursos.stream()
+                .filter(curso -> curso.getAlunos() > 100)
+                .mapToInt(Curso::getAlunos)
+                .sum();
+
+        System.out.println(sum);
+    }
+
+
 }

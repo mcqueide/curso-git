a7e3aed1827aac5e49a927a44bafeb09fa33ffed
mcqueide <mcqueide@gmail.com>
2017-05-10 18:15:40 -0300

    Adicionando exercícios da aula 2

--- alura-soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ alura-soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -3,9 +3,12 @@ package br.com.caelum.estoque.ws;
 import java.util.List;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-
+import javax.xml.ws.ResponseWrapper;
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
@@ -17,9 +20,19 @@ public class EstoqueWS {
 	
 	@WebMethod(operationName="todosOsItens")
 	@WebResult(name="itens")
+	@ResponseWrapper(localName="getItens")
 	public ListaItens getItens(){
 		System.out.println("Chamando getItens()");
 		List<Item> todosItens = dao.todosItens();
 		return new ListaItens(todosItens);
 	}
+	
+	@WebMethod(operationName="todosOsItensComFiltro") 
+    @WebResult(name="itens")
+    public ListaItens getItensComFiltro(@WebParam(name="filtros") Filtros filtros) {
+        System.out.println("Chamando getItens()");
+        List<Filtro> lista = filtros.getLista();
+        List<Item> itensResultado = dao.todosItens(lista);
+        return new ListaItens(itensResultado);
+    }
 }

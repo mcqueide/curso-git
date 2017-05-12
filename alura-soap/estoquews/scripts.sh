e201bfd93ee05cad37137bdb26286c88b1d2f7c8
mcqueide <mcqueide@gmail.com>
2017-05-12 14:31:19 -0300

    Adicionando comando

--- alura-soap/estoquews/scripts.sh
+++ alura-soap/estoquews/scripts.sh
@@ -6,3 +6,6 @@ sh wsconsume.sh -k -n -o /Users/nico/workspace/estoque-web/src /Users/nico/works
 
 # geração das classe do projeto cliente do WebService
 wsimport -s src -p br.com.caelum.estoque.cliente http://localhost:8080/estoquews-web/EstoqueWS?wsdl
+
+# geração das classes do projeto cliente para os Correios
+wsimport -s src/ -p br.com.caelum.correios.soap http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?WSDL

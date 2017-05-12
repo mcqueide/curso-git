a4df1b0b12f7bf003e6719145d4a44e60178aca5
mcqueide <mcqueide@gmail.com>
2017-05-12 13:58:32 -0300

    Adicionando projeto SOAP com contract first

--- alura-soap/estoquews/scripts.sh
+++ alura-soap/estoquews/scripts.sh
@@ -1,2 +1,5 @@
 # geração de contrato wsdl
 wsgen -cp bin br.com.caelum.estoque.ws.EstoqueWS -wsdl
+
+# geração de projeto SOAP com contract first pelo wildfly
+sh wsconsume.sh -k -n -o /Users/nico/workspace/estoque-web/src /Users/nico/workspace/estoque-web/src/EstoqueWSServiceCap5.wsdl
--- alura-soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ alura-soap/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,10 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
 import javax.xml.ws.ResponseWrapper;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
@@ -17,6 +21,7 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
+@SOAPBinding(style=Style.DOCUMENT,use=Use.LITERAL,parameterStyle=ParameterStyle.BARE)
 public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();

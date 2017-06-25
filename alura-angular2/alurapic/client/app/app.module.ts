15817077928a2c9550475f2d35c4534eb5b5eb1c
mcqueide <mcqueide@gmail.com>
2017-06-25 12:19:51 -0300

    Fim do vídeo 2 aula 4 - Reorganizando os components

--- alura-angular2/alurapic/client/app/app.module.ts
+++ alura-angular2/alurapic/client/app/app.module.ts
@@ -3,13 +3,16 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpModule} from '@angular/http';
 
-import {AppComponent} from './app.component';
 import {FotoModule} from './foto/foto.module';
 import {PainelModule} from './painel/painel.module';
 
+import {AppComponent} from './app.component';
+import {ListagemComponent} from './listagem/listagem.component';
+import {CadastroComponent} from './cadastro/cadastro.component';
+
 @NgModule({
     imports: [BrowserModule,FotoModule,HttpModule,PainelModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent,ListagemComponent,CadastroComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}

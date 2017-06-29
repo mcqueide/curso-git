e7a9045ae88b1056156f5e5cd79268ea688466d8
mcqueide <mcqueide@gmail.com>
2017-06-29 18:16:04 -0300

    Fim vídeo 1 aula 6 - Adicionando formulário de cadastro

--- alura-angular2/alurapic/client/app/cadastro/cadastro.component.ts
+++ alura-angular2/alurapic/client/app/cadastro/cadastro.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {FotoComponent} from '../foto/foto.component';
 
 @Component({
     moduleId: module.id,
     selector: 'cadastro',
     templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent{}
+export class CadastroComponent{
+
+    foto: FotoComponent = new FotoComponent();
+
+    cadastrar(){
+        console.log(this.foto);
+    }
+}
--- alura-angular2/alurapic/client/app/foto/foto.component.ts
+++ alura-angular2/alurapic/client/app/foto/foto.component.ts
@@ -9,4 +9,5 @@ export class FotoComponent{
 
     @Input() titulo: string;
     @Input() url: string;
+    descricao: string;
 }

c0526a758c195a8758c8ab20c33ccfea31d76480
mcqueide <mcqueide@gmail.com>
2017-07-01 17:14:25 -0300

    Fim do vídeo 4 aula 6 - Configurando o ngModel

--- alura-angular2/alurapic/client/app/app.module.ts
+++ alura-angular2/alurapic/client/app/app.module.ts
@@ -2,6 +2,7 @@ import 'rxjs/add/operator/map';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HttpModule} from '@angular/http';
+import { FormsModule} from '@angular/forms';
 
 import { routing } from './app.routes';
 
@@ -13,7 +14,7 @@ import {ListagemComponent} from './listagem/listagem.component';
 import {CadastroComponent} from './cadastro/cadastro.component';
 
 @NgModule({
-    imports: [BrowserModule,FotoModule,HttpModule,PainelModule, routing],
+    imports: [BrowserModule,FotoModule,HttpModule,PainelModule, routing,FormsModule],
     declarations: [AppComponent,ListagemComponent,CadastroComponent],
     bootstrap: [AppComponent]
 })

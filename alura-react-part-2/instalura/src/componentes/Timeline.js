9764f3975d10ae309a5c9749b8ab37e001dadddf
mcqueide <mcqueide@gmail.com>
2018-05-01 11:12:28 -0300

    Finalizando aula 3

--- alura-react-part-2/instalura/src/componentes/Timeline.js
+++ alura-react-part-2/instalura/src/componentes/Timeline.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
 export default class Timeline extends Component {
-    render(){
-        return (
-        <div className="fotos container">
-          <FotoItem/>
-          <FotoItem/>
-        </div>            
-        );
-    }
+
+  constructor() {
+    super();
+    this.state = { fotos: [] };
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+      .then(response => response.json())
+      .then(fotos => {
+        this.setState({ fotos: fotos });
+      });
+  }
+
+  render() {
+    return (
+      <div className="fotos container">
+        {
+          this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} />)
+        }
+      </div>
+    );
+  }
 }
--- alura-react-part-2/instalura/src/index.js
+++ alura-react-part-2/instalura/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './componentes/Login';
+import Logout from './componentes/Logout';
 import registerServiceWorker from './registerServiceWorker';
+import {Router,Route,browserHistory} from 'react-router';
 
 import './css/reset.css';
 import './css/timeline.css';
+import './css/login.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function verificaAutenticacao(nextState,replace) {
+  if(localStorage.getItem('auth-token') == null) {
+    replace('/?msg=você precisa estar logado para acessar o endereço');
+  }
+}
+
+ReactDOM.render(
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={Login} />
+      <Route path="/timeline" component={App} onEnter={verificaAutenticacao}/>
+      <Route path="/logout" component={Logout}/>
+    </Router>
+  ), document.getElementById('root'));
 registerServiceWorker();

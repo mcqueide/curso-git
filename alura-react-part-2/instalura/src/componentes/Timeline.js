708dc78c23ab3fcc6f4df90fd0e9eb61521aea84
mcqueide <mcqueide@gmail.com>
2018-05-07 19:18:39 -0300

    Finalizando video 1 aula 2

--- alura-react-part-2/instalura/src/componentes/Timeline.js
+++ alura-react-part-2/instalura/src/componentes/Timeline.js
@@ -10,8 +10,8 @@ export default class Timeline extends Component {
   }
 
   componentWillMount() {
-    this.props.store.subscribe(fotos => {
-      this.setState({ fotos });
+    this.props.store.subscribe(() => {
+      this.setState({ fotos: this.props.store.getState() });
     })
   }
 
@@ -23,7 +23,11 @@ export default class Timeline extends Component {
     } else {
       urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
     }
-    this.props.store.lista(urlPerfil);
+
+    const listaFixa = [{"urlPerfil":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"07/05/2018 09:31","urlFoto":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-15/e35/14482111_1635089460122802_8984023070045896704_n.jpg?ig_cache_key=MTM1MzEzNjM4NzAxMjIwODUyMw%3D%3D.2","id":1,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"},{"urlPerfil":"https://instagram.fcgh10-1.fna.fbcdn.net/t51.2885-19/11199408_569104449895751_1837574990_a.jpg","loginUsuario":"alots","horario":"07/05/2018 09:31","urlFoto":"https://instagram.fcgh9-1.fna.fbcdn.net/t51.2885-15/e35/15276770_381074615568085_8052939980646907904_n.jpg?ig_cache_key=MTM5ODY4MDMyNjYyMDA1MDE4OQ%3D%3D.2","id":2,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto"}];
+    
+    this.props.store.dispatch({type:'LISTAGEM',fotos:listaFixa});
+    //this.props.store.lista(urlPerfil);
   }
 
   componentDidMount() {

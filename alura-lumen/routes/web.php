769d6f1435c7309fd7915da63a7657aca66e37e5
John Mc.Queide Clemente <mcqueide@gmail.com>
2020-02-02 12:11:53 -0300

    - Aula 5.

--- alura-lumen/routes/web.php
+++ alura-lumen/routes/web.php
@@ -15,7 +15,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api'], function () use ($router) {
+$router->group(['prefix' => 'api', "middleware" => 'auth'], function () use ($router) {
     $router->group(['prefix' => 'series'], function () use ($router) {
         $router->get('', 'SeriesController@index');
         $router->get('{id}', 'SeriesController@show');
@@ -34,3 +34,5 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->delete('{id}', 'EpisodiosController@destroy');
     });
 });
+
+$router->post('/api/login', 'TokenController@gerarToken');

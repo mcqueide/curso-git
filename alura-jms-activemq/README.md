b3791edb958f33f9cfe375470c8fb73b19a15676
John Mc.Queide Clemente <john.clemente@tivit.com>
2020-04-15 08:14:06 -0300

    - Adicionando curso jms-activemq da alura até a aula 4 parte 1.

---
+++ alura-jms-activemq/README.md
@@ -0,0 +1,11 @@
+# alura-jms-activemq
+
+Esse é um projeto com base no curso da alura JMS e ActiveMQ para praticar o conteúdo do curso.
+
+## configurações
+
+- Baixar o arquivo do [ActiveMQ](https://s3.amazonaws.com/caelum-online-public/jms/apache-activemq-5.12.0-bin.zip)
+- No windows é necessário executar o `InstallService.bat` antes que está na bin/win32 ou bin/win64
+- Iniciar o ActiveMQ com o script com o comando `bin\activemq active` (windows) ou `sh bin/activemq console` (linux)
+- Abrir a página `http://localhost:8161/admin`, login: admin e senha: admin
+- Lembrar de adicionar o **activemq-all-5.12.0.jar** no classpath.
---
+++ alura-jms-activemq/src/main/java/br/com/mcqueide/alurajmsactivemq/TesteProdutor.java
@@ -0,0 +1,40 @@
+package br.com.mcqueide.alurajmsactivemq;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteProdutor {
+
+    public static void main(String[] args) throws Exception{
+
+        InitialContext context = new InitialContext();
+
+        //imports do package javax.jms
+        ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+        Connection connection = factory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination fila = (Destination) context.lookup("financeiro");
+
+        MessageProducer producer = session.createProducer(fila);
+
+        for (int i = 0; i < 1000; i++) {
+            Message message = session.createTextMessage("<produto><id>"+i+"</id></produto>");
+            producer.send(message);
+        }
+
+        new Scanner(System.in).nextLine(); //parar o programa para testar a conexao
+
+        session.close();
+        connection.close();
+        context.close();
+    }
+}
